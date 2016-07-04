3a42caa5faf835efa40f46526aa6b1ee9b013019
ren-y <[ren1902@gmail.com]>
2016-07-03 23:15:32 -0700

    Pizza Restaurant assignment

---
+++ TestLynda/TestLynda/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TestLynda
+//
+//  Created by reena on 02/07/16.
+//  Copyright © 2016 Reena. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
---
+++ skdsn/skdsn/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  skdsn
+//
+//  Created by reena on 30/06/16.
+//  Copyright © 2016 Reena. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
